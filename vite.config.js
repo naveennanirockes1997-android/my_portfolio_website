cf5a3accf6d6e7f30efbf14027eac145be387690
naveennanirockes1997-android <naveennanirockes1997@gmail.com>
2026-02-12 10:15:32 +0530

    Fix base path for Renderss

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.VITE_BASE_PATH || "/my_portfolio_website",
+  base: '/',
 
   plugins: [
     react(),
